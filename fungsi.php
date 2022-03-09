972babbe7dff4378cf7c5f872ed695100561c6db
Zey21 <mukhammad.zaidan.abdulloh@gmail.com>
2022-03-10 00:45:54 +0700

    Tugas 2 no 2 dan no 3

---
+++ fungsi.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <h1>Tugas 3</h1>
+    <?php
+
+    function Jumlah($Pertama,$Kedua){
+        echo"<h2>Penjumlahan</h2><br>Penjumlahan dari ".$Pertama." + ".$Kedua." = ".$Pertama + $Kedua."<br>";
+    }
+    Jumlah(12,23);
+    Jumlah(23,34);
+    Jumlah(34,45);
+    ?>
+</html>
---
+++ variable.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <h1>Tugas 2</h1>
+    <?php
+    $numbP = 4;
+    $numbK = 3;
+        echo"<h2>Penjumlahan</h2><br>Penjumlahan dari ".$numbP." + ".$numbK." = ".$numbP + $numbK."<br>";
+        echo"<h2>Pengurangan</h2><br>Pengurangan dari ".$numbP." - ".$numbK." = ".$numbP - $numbK."<br>";
+        echo"<h2>Perkalian</h2><br>Perkalian dari ".$numbP." x ".$numbK." = ".$numbP * $numbK."<br>";
+    ?>
+</html>
